1143edb14fb54fa96500958bef6ffd7d5863e580
pannaincze <pannaincze@gmail.com>
2023-10-17 11:30:43 +0200

    add application

---
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/SportBuddyFinderApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sportbuddyfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBuddyFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportBuddyFinderApplication.class, args);
+	}
+
+}
