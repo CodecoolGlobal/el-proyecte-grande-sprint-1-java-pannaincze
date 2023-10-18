d698b321e8889c915c07f07d3df792f7bfc5855b
GagoRobi <gagorobi96@gmail.com>
2023-10-18 19:28:00 +0200

    add corsConfiguration for requests, fix bootstrap

--- sport-buddy-finder/src/main/ui/my-app/src/pages/Register.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/pages/Register.jsx
@@ -1,36 +1,57 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
 export default function (){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [interests, setInterests] = useState([]);
     function handleRegistration(){
-
+        console.log(name);
     }
-
-
-
+    function checkboxHandling(target){
+        console.log(target.checked);
+        console.log(target.value);
+    }
+    useEffect(()=>{
+        fetchSports();
+    },[])
+    async function fetchSports(){
+        const response = fetch("http://localhost:8080/activities/categories", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                //setJoke(data[0].joke);
+                console.log(data);
+            })
+            .catch((error) => console.log(error));
+    }
     return(
         <div>
-            <form>
-                <div className="mb-3">
-                    <label for="name" class="form-label">Name: </label>
-                    <input id="name" placeholder="John Doe"></input>
+            <form className="container">
+                <div className="mb-3 ">
+                    <label htmlFor="name" className="form-label">Name: </label>
+                    <input onChange={event => setName(event.target.value) } id="name" placeholder="John Doe"></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address: </label>
-                    <input id="email" type="email" placeholder="example@gmail.com"></input>
+                    <input onChange={event => setEmail(event.target.value) } id="email" type="email" placeholder="example@gmail.com"></input>
                 </div>
                 <div className="mb-3">
-                    <label for="password" class="form-label">Password: </label>
-                    <input id="password" type={"password"} placeholder="********"></input>
+                    <label htmlFor="password" className="form-label">Password: </label>
+                    <input onChange={event => setPassword(event.target.value) } id="password" type={"password"} placeholder="********"></input>
                 </div>
-
-                <div className="mb-3 form-check">
+                <div className="mb-3 container ">
                     <label>Sport interests:</label><br/>
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+                    <input onClick={event =>  checkboxHandling(event.target)} type="checkbox" className="form-check-input" id="exampleCheck1" value={"Sport1"}></input>
                     <label className="form-check-label" htmlFor="exampleCheck1">Check1</label>
                     <input type="checkbox" className="form-check-input" id="exampleCheck2"></input>
                     <label className="form-check-label" htmlFor="exampleCheck2">Check2</label>
                     <input type="checkbox" className="form-check-input" id="exampleCheck3"></input>
                     <label className="form-check-label" htmlFor="exampleCheck3">Check3</label>
                 </div>
-                <button> Save </button>
+                <button type={"button"} onClick={handleRegistration}> Save </button>
             </form>
         </div>
 
