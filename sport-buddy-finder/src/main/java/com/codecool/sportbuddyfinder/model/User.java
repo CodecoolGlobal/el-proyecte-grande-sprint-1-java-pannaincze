4c450d1e3dc009d36812fa55e5752c35d74c3574
gavaller-vid <gavalleratilla@gmail.com>
2023-10-18 10:40:01 +0200

    add: interests field to User, initialize it with picked interests at registration. Add add methods for more interests.

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/User.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/User.java
@@ -1,7 +1,12 @@
 package com.codecool.sportbuddyfinder.model;
 
+import com.codecool.sportbuddyfinder.model.activity.Activity;
+import com.codecool.sportbuddyfinder.model.activity.Sport;
+
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class User {
@@ -11,15 +16,15 @@ public class User {
     private String password;
     private LocalDate birthDate;
     private String profilePicURL;
-    //private Set<T> interests;
-
-    public User(String name, String email, String password, LocalDate birthDate) {
+    private final Set<Sport> interests;
+    private Set<Activity> postedActivities;
+    public User(String name, String email, String password, LocalDate birthDate, List<Sport> interests) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.birthDate = birthDate;
         this.profilePicURL = "https://thumbs.dreamstime.com/b/head-silhouette-face-front-view-human-elegant-part-human-vector-illustration-79409597.jpg";
-        //this.interests = new HashSet<T>(); write getter for this!
+        this.interests = new HashSet<>(interests);
         this.userID = UUID.randomUUID();
     }
 
@@ -65,5 +70,15 @@ public class User {
 
     public void setProfilePicURL(String profilePicURL) {
         this.profilePicURL = profilePicURL;
+    }
+
+    public Set<Sport> getInterests() {
+        return interests;
+    }
+    public void addInterest(Sport sport){
+        interests.add(sport);
+    }
+    public void addInterests(Set<Sport> interests){
+        this.interests.addAll(interests);
     }
 }
