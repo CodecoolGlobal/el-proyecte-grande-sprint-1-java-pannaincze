abe163a632535652e6f6d133e26bb4ac0956e19e
pannaincze <pannaincze@gmail.com>
2023-10-19 11:08:12 +0200

    add styling for the main page

--- sport-buddy-finder/src/main/ui/my-app/src/components/ActivityCards.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/components/ActivityCards.jsx
@@ -1,18 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Card} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 
 export default function ActivityCards({activities}) {
     return (
-        <div className="ActivityCards">
+        <div className="ActivityCards" style={{ display: "flex", flexWrap: "wrap"}}>
         {activities.map((activity) => (
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem', margin: "0.5rem" }}>
+                <Card.Header as="h5">{activity.sport}</Card.Header>
                 <Card.Body>
                     <Card.Title>{activity.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{activity.sport}</Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">{activity.location}</Card.Subtitle>
                     <Card.Text>
                         {`${activity.appliedUsers.length}/${activity.maxPeopleToFind}`}
                     </Card.Text>
+                    <Button variant="primary">See more</Button>
                 </Card.Body>
             </Card>
             ))}
