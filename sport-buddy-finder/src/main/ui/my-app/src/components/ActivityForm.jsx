17a1b4abf4f54d706615373d7fc2178241da589b
Lichtner Levente <lichtner.levente@gmail.com>
2023-10-19 13:00:41 +0200

    add keys to sport category options

--- sport-buddy-finder/src/main/ui/my-app/src/components/ActivityForm.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/components/ActivityForm.jsx
@@ -37,8 +37,7 @@ export const ActivityForm = ({handleSave, onCancel, sportCategories}) => {
                 <Form.Label htmlFor="sport">Sport:</Form.Label>
                 <Form.Select onChange={(e) => setSport(e.target.value)} id="type" required={true}>
                     <option value="" disabled selected>Select your option</option>
-                    {sportCategories.map((category, index) => <option value={category}>{category}</option>
-                    )}
+                    {sportCategories.map((category) => <option value={category} key={category}>{category}</option>)}
                 </Form.Select>
 
                 <Form.Label htmlFor="description">Description:</Form.Label>
