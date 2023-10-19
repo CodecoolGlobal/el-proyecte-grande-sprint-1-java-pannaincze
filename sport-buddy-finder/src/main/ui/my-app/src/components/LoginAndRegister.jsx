b7f7295bd3c0ac53fbb4525ecc73c07719f3064d
GagoRobi <gagorobi96@gmail.com>
2023-10-19 13:00:59 +0200

    add link to login button

--- sport-buddy-finder/src/main/ui/my-app/src/components/LoginAndRegister.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/components/LoginAndRegister.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function (){
     return(
         <div>
-        <button>Login</button>
+            <Link to={"/login"}><button>Login</button></Link>
             <Link to={"/register"}><button>Register</button></Link>
     </div>
     )
