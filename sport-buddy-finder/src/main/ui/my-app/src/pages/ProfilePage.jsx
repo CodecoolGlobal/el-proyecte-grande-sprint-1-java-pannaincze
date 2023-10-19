ca97f28c00441aa9f33be58d019a9620fd299bac
gavaller-vid <gavalleratilla@gmail.com>
2023-10-19 11:35:06 +0200

    refactor: ProfilePage

--- sport-buddy-finder/src/main/ui/my-app/src/pages/ProfilePage.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/pages/ProfilePage.jsx
@@ -1,8 +1,18 @@
 import {useState, useEffect} from "react";
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+export default function ProfilePage({userID}){
 
-export default function ProfilePage(){
-
-    const [user, setUser] = useState({name:"",email:"",password:"",birthDate:""})
+    const [user, setUser] = useState({
+        name:"",
+        email:"",
+        password:"",
+        birthDate:"",
+        userID:"",
+        profilePicURL:"",
+        interests: [],
+        postedActivities: []})
     async function fetchUser(userID){
         fetch(`http://localhost:8080/${userID}`,{
             method: "GET",
@@ -12,6 +22,15 @@ export default function ProfilePage(){
             .catch((error) => console.log(error));
     }
     useEffect(()=> {
-        fetchUser(userId)
+        fetchUser(userID)
     },[])
+    return (
+        <div>
+            <Container>
+                    <Col xs={6} md={4}>
+                        <Image src={user.profilePicURL} rounded />
+                    </Col>
+            </Container>
+        </div>
+    )
 }
