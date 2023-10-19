e38ad142d0c44dd48936de875840cf281fa8729c
gavaller-vid <gavalleratilla@gmail.com>
2023-10-19 13:22:08 +0200

    add: html for user info

--- sport-buddy-finder/src/main/ui/my-app/src/pages/ProfilePage.jsx
+++ sport-buddy-finder/src/main/ui/my-app/src/pages/ProfilePage.jsx
@@ -14,7 +14,7 @@ export default function ProfilePage({userID}){
         interests: [],
         postedActivities: []})
     async function fetchUser(userID){
-        fetch(`http://localhost:8080/${userID}`,{
+        fetch(`http://localhost:8080/users/${userID}`,{
             method: "GET",
         })
             .then((response) => response.json())
@@ -22,6 +22,7 @@ export default function ProfilePage({userID}){
             .catch((error) => console.log(error));
     }
     useEffect(()=> {
+        console.log(userID)
         fetchUser(userID)
     },[])
     return (
@@ -31,6 +32,18 @@ export default function ProfilePage({userID}){
                         <Image src={user.profilePicURL} rounded />
                     </Col>
             </Container>
+            <h1>{user.name}</h1>
+            <h2>{user.email}</h2>
+            <ul>
+                {user.interests.map((interest) => {
+                    return <li>{interest}</li>
+                })}
+            </ul>
+            <ul>
+                {user.postedActivities.map((activity) => {
+                    return <li>{activity}</li>
+                })}
+            </ul>
         </div>
     )
 }
