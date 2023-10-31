8ba1e0964aed6f1d36423bc77e2975e344a7d8ed
pannaincze <pannaincze@gmail.com>
2023-10-31 10:31:32 +0100

    refactor sport table, rename id column to sport_id

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/entities/Sport.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/entities/Sport.java
@@ -1,10 +1,7 @@
 package com.codecool.sportbuddyfinder.model.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,7 @@ public class Sport {
 
     @Id
     @GeneratedValue
+    @Column(name = "sport_id")
     private long id;
     private String name;
 }
