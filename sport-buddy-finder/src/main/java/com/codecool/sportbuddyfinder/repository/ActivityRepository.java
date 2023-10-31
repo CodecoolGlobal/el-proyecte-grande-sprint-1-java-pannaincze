79dd7b49ae82cc5a161ca29004739c9fb22f5119
GagoRobi <gagorobi96@gmail.com>
2023-10-31 11:08:19 +0100

    add todo

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/repository/ActivityRepository.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/repository/ActivityRepository.java
@@ -4,6 +4,11 @@ import com.codecool.sportbuddyfinder.model.activity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-
+//Set<Activity> getAllActivities();
+//    Activity getActivityById(UUID uuid);
+//    Activity addNewActivity(Activity activity);
+//    UUID deleteActivityById(UUID uuid);
+//    Activity updateActivityById(UUID uuid);
+//    Activity addUserToActivity(UUID activityId, User user);
 
 }
