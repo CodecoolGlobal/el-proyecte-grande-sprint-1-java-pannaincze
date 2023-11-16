3f487f5c9e688b60dd8bb37edc596ba587b34e99
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2023-11-16 10:48:22 +0100

    Improve RestExceptionHandler

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/exception/RestExceptionHandler.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/exception/RestExceptionHandler.java
@@ -1,20 +1,21 @@
 package com.codecool.sportbuddyfinder.exception;
 
+import com.codecool.sportbuddyfinder.model.apiError.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler {
-    @ExceptionHandler(ActivityNotFoundException.class)
-    public ResponseEntity<Object> handleActivityNotFoundException(Exception e){
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({ActivityNotFoundException.class, UserNotFoundException.class})
+    public ResponseEntity<Object> handleEntityNotFoundException(ActivityNotFoundException ex){
+        return new ResponseEntity<>(new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e){
+    public ResponseEntity<Object> handleException(Exception e){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error :(");
     }
 }
