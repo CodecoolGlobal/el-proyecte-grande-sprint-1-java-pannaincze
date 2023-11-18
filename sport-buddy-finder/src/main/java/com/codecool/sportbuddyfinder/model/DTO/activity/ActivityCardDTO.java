6f0cf5b9bcaf60791effa8573ae39ec82e4d6959
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2023-11-18 05:15:30 +0100

    remove unnecessary code

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/DTO/activity/ActivityCardDTO.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/DTO/activity/ActivityCardDTO.java
@@ -15,17 +15,6 @@ public class ActivityCardDTO {
     private String image;
     private Integer numberOfAppliedUsers;
 
-    public ActivityCardDTO(long id, String title, Sport sport, String location, Integer minPeopleToFind, Integer maxPeopleToFind, String image, Integer numberOfAppliedUsers) {
-        this.id = id;
-        this.title = title;
-        this.sport = sport;
-        this.location = location;
-        this.minPeopleToFind = minPeopleToFind;
-        this.maxPeopleToFind = maxPeopleToFind;
-        this.image = image;
-        this.numberOfAppliedUsers = numberOfAppliedUsers;
-    }
-
     public ActivityCardDTO(Activity activity) {
         this.id = activity.getId();
         this.title = activity.getTitle();
