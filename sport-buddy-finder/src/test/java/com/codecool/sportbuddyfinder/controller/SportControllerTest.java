03d7deb3d4becbcaa34b601e195896b59035d72a
gavaller-vid <gavalleratilla@gmail.com>
2023-11-28 14:11:31 +0100

    add: web layer integration test:  to SportControllerTest. test: getAll should return an array

--- sport-buddy-finder/src/test/java/com/codecool/sportbuddyfinder/controller/SportControllerTest.java
+++ sport-buddy-finder/src/test/java/com/codecool/sportbuddyfinder/controller/SportControllerTest.java
@@ -54,4 +54,16 @@ class SportControllerTest {
                 .andExpect(jsonPath("$[1].id").value(2))
                 .andExpect(jsonPath("$[1].name").value("Dancing"));
     }
+    @Test
+    void getAllShouldReturnSportsArrayFromService() throws Exception {
+        List<Sport> mockSports = Arrays.asList(new Sport(1,"Boxing"),new Sport(2,"Dancing"));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writer().with(SerializationFeature.INDENT_OUTPUT);
+        String jsonSports = objectWriter.writeValueAsString(mockSports);
+        when(sportService.getAll()).thenReturn(mockSports);
+        this.mockMvc.perform(get("/sports")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
 }
