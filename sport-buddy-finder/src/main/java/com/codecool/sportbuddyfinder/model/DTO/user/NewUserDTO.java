df559212df5fc169337b6b5f64dcc0e819f4d752
gavaller-vid <gavalleratilla@gmail.com>
2023-11-29 14:03:42 +0100

    refactor: NewUserDTO. add: @JsonFormat to configure how values of properties are serialized or deserialized.

--- sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/DTO/user/NewUserDTO.java
+++ sport-buddy-finder/src/main/java/com/codecool/sportbuddyfinder/model/DTO/user/NewUserDTO.java
@@ -1,9 +1,11 @@
 package com.codecool.sportbuddyfinder.model.DTO.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
 
 @Builder
-public record NewUserDTO(String name, String email, String password, int[] interests, LocalDate date) {
+public record NewUserDTO(String name, String email, String password, int[] interests,
+                         @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING) LocalDate date) {
 }
